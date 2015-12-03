8d3891305911a405d4482b3d2a91cde681154dae
admande <adam.mandeville@gmail.com>
2015-12-03 14:37:21 -0500

    refactored, with infinite scroll

--- public/js/sprouts.js
+++ public/js/sprouts.js
@@ -1,32 +1,20 @@
-// YOUR CODE GOES HERE
-  var linkNum = function(link){
-    return /\d+/.exec(link);
-  };
+var linkNum = function(link){
+   return /\d+/.exec(link);
+ };
 
+ var pageAppend = function(data) {
+   for(var i = 0; i < 10; i++){
+     $(".tweets").append("<li class=tweet>" + data[i]["text"] + "<div class=user>" + data[i]['username'] + "</div></li>");
+   }
+ }
 
-$(".more-sprouts").on("click", function(event) {
-  event.preventDefault();
-  var pageURL = $('a').attr("href");
-  var pageNUM = parseInt(linkNum(pageURL)[0]);
+ var pageIncrement = function(number){
+   $('a').attr('href', '/tweets?page=' + (number + 1));
+ }
 
-  var request = $.ajax({
-  method: "GET",
-  url: "/tweets.json?page=" + pageNUM
-
-  });
-
-
-  request.done(function(data) {
-    for(var i = 0; i < 10; i++){
-      $(".tweets").append("<li class=tweet>" + data[i]["text"] + "<div class=user>" + data[i]['username'] + "</div></li>");
-    }
-    $('a').attr('href', '/tweets?page=' + (pageNUM + 1));
-  });
-
-});
 
 $(window).scroll(function () {
-  if ($(window).scrollTop() >= $(document).height() - $(window).height() - 10) {
+  if ($(window).scrollTop() >= $(document).height() - $(window).height() - 1) {
     var pageURL = $('a').attr("href");
     var pageNUM = parseInt(linkNum(pageURL)[0]);
 
@@ -35,13 +23,9 @@ $(window).scroll(function () {
     url: "/tweets.json?page=" + pageNUM
 
     });
+   request.done(pageAppend);
+   pageIncrement(pageNUM);
 
 
-    request.done(function(data) {
-      for(var i = 0; i < 10; i++){
-        $(".tweets").append("<li class=tweet>" + data[i]["text"] + "<div class=user>" + data[i]['username'] + "</div></li>");
-      }
-      $('a').attr('href', '/tweets?page=' + (pageNUM + 1));
-    });
   }
 });
