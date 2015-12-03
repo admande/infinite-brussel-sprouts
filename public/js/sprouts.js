890401d8401da0d500c607fb5c3184d0bcf499c4
admande <adam.mandeville@gmail.com>
2015-12-03 14:25:16 -0500

    added infinite scroll

--- public/js/sprouts.js
+++ public/js/sprouts.js
@@ -24,3 +24,24 @@ $(".more-sprouts").on("click", function(event) {
   });
 
 });
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() >= $(document).height() - $(window).height() - 10) {
+    var pageURL = $('a').attr("href");
+    var pageNUM = parseInt(linkNum(pageURL)[0]);
+
+    var request = $.ajax({
+    method: "GET",
+    url: "/tweets.json?page=" + pageNUM
+
+    });
+
+
+    request.done(function(data) {
+      for(var i = 0; i < 10; i++){
+        $(".tweets").append("<li class=tweet>" + data[i]["text"] + "<div class=user>" + data[i]['username'] + "</div></li>");
+      }
+      $('a').attr('href', '/tweets?page=' + (pageNUM + 1));
+    });
+  }
+});
